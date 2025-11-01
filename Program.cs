c8915806971cf032c587bfe0cb8bb03ef2d1654f
Manish <manish.mishra.261001@gmail.com>
2025-11-01 19:25:47 +0530

    program.cs - logic added

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    
+    static void Main()
+    {
+                Console.Write("Enter the number of rows for the triangle: ");
+                 if (!int.TryParse(Console.ReadLine(), out int n) || n <= 0)
+        {
+            Console.WriteLine("Please enter a positive integer.");
+            return;
+        }
+        for(int i=1;i<=n;i++)
+        {
+            Console.Write(new string(' ', n - i));
+            Console.WriteLine(new string('*', 2 * i - 1));
+        }
+
+    }
+}
